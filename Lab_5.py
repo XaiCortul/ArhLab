670b5bd51c09aa05dc258e9d3c91af0c2a44efed
XaiCortul <xaicortul@gmail.com>
2024-05-19 18:39:46 +0300

    Add files via upload

---
+++ Lab_5.py
@@ -0,0 +1,17 @@
+class Product:
+	def __init__(self, name, price, type):
+    	self.name = name
+    	self.price = price
+    	self.type = type
+
+class ProductSearch:
+	def search_product(self, product_list, query):
+    	pass
+
+class ProductDisplay:
+	def display_product(self, product):
+    	pass
+
+class ProductOrder:
+	def order_product(self, product, quantity):
+    	pass
